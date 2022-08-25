49cbffc74a562838d9aee160e752f75c737c10a5
Yafei Liu <yafeiliu@yafeis-air.myfiosgateway.com>
2022-08-25 13:13:19 -0400

    Added User.js file in server

--- server/models/Users.js
+++ server/models/Users.js
@@ -1,2 +1,30 @@
 const { Schema, model } = require("mongoose");
 const bcrypt = require("bcrypt");
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/.+@.+\..+/, "Must match an email address!"],
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 4,
+  },
+  thoughts: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Thought",
+    },
+  ],
+});
+
+module.exports = User;
